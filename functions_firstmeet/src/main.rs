4c71414deb4b9f8e7de012aa8ffa0f1a9c99ac23
LTH-XZH <2192894594@qq.com>
2026-07-24 18:31:42 +0800

    带有返回值的函数&语句和表达式的区别

--- functions_firstmeet/src/main.rs
+++ functions_firstmeet/src/main.rs
@@ -1,8 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-
-    another_function(5);
-}
-fn another_function(x : i32) {
+fn main(){
+    let x = plus_one(5);
     println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
