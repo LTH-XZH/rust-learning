dd64a203437f3eac1673e11d683419ad80da9f35
LTH-XZH <2192894594@qq.com>
2026-07-30 09:55:18 +0800

    上次使用递归迭代，本次使用循环迭代

--- Fibonacci/src/main.rs
+++ Fibonacci/src/main.rs
@@ -4,9 +4,20 @@ fn fibonacci(n: u32) -> u32 {
     if n <= 1 {
         return n;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    for _ in 2..n+1 {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    c
 }
+
 fn main() {
+    println!("Enter a number to calculate its Fibonacci:");
     let mut n: String = String::new();
     io::stdin().read_line(&mut n).unwrap();
     let n: u32 = n.trim().parse().unwrap();
